053a3330693f462bedca1f6788eb4805223ccb90
Arosmita Mukherjee <mmeghla244@gmail.com>
2026-05-21 10:19:14 +0530

    no distraction start

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log("lets make my first project");
 
 async function getSongs() {
-    let a = await fetch("http://127.0.0.1:3000/songs/");
+    let a = await fetch("http://127.0.0.1:3000/songs/")
     let response = await a.text();
     console.log(response)
     let div = document.createElement("div")
@@ -11,7 +11,8 @@ async function getSongs() {
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
         if (element.href.endsWith(".mp3")) {
-            songs.push(element.href)
+            songs.push(element.href.split("%5Csongs%5C")[1]);
+            // songs.push(element.href);
         }
     }
     return songs
@@ -20,18 +21,17 @@ async function main() {
     let songs = await getSongs()
     console.log(songs)
 
-    let songUL = document.querySelector(".libary").getElementsByTagName("ul")[0]
+    let songUL = document.querySelector(".songlist").getElementsByTagName("ul")[0]
     for (const song of songs) {
-        songUL.innerHTML = songUL.innerHTML + `<li> ${song} </li>`;
-        
+        songUL.innerHTML = songUL.innerHTML + `<li> ${song.replaceAll("%20", "  ")} </li>`;
     }
 
     var audio = new Audio(songs[0]);
-    // audio.play(); 
+    // audio.play();
 
     audio.addEventListener("loadeddata", () => {
-        console.log(audio.duration,audio.currentSrc , audio.currentTime)
+        console.log(audio.duration, audio.currentSrc, audio.currentTime)
     });
 }
-   
+
 main() 
